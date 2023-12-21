f74cf7587ecaa4f747a6e3081397412ecb1ce4e5
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2023-12-20 22:35:18 -0700

    fix up the blur

--- panda.config.ts
+++ panda.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
     extend: {},
   },
 
+  jsxFramework: "react",
+
   patterns: {
     extend: {
       shadowPart: {
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -5,10 +5,12 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonIcon,
 } from "@ionic/react";
 import cover from "../Book Cover.jpg";
 import { css, cx } from "../../styled-system/css";
-import { bleed, shadowPart } from "../../styled-system/patterns";
+import { bleed, shadowPart, hstack } from "../../styled-system/patterns";
+import { arrowForwardCircleOutline } from "ionicons/icons";
 
 const Tab1: React.FC = () => {
   return (
@@ -41,25 +43,59 @@ const Tab1: React.FC = () => {
               css({
                 position: "absolute",
                 zIndex: -1,
+                top: "-20%",
               })
             )}
           />
-          <IonImg
-            className={cx(
-              shadowPart({
-                part: "image",
-                borderRadius: "sm",
-              }),
-              css({
-                padding: 16,
-              })
-            )}
-            src={cover}
-            alt="Cover"
-          />
+          <PromotedPublicationCard />
         </div>
       </IonContent>
     </IonPage>
+  );
+};
+
+const PromotedPublicationCard = () => {
+  return (
+    <div
+      className={css({
+        border: "1px solid rgba(208, 213, 221, 0.4)",
+        bg: "black",
+        borderRadius: "lg",
+        margin: 6,
+        padding: 4,
+      })}
+    >
+      <IonImg
+        className={cx(
+          shadowPart({
+            part: "image",
+            borderRadius: "sm",
+            maxH: "280px",
+          }),
+          css({
+            pb: 4,
+          })
+        )}
+        src={cover}
+        alt="Cover"
+      />
+      <div className={hstack()}>
+        <div className={css({ fontWeight: "bold" })}>
+          <div className={css({ fontSize: "sm" })}>
+            NYPL & WYNC Book Club’s November Pick
+          </div>
+          <div
+            className={css({
+              fontSize: "2xl",
+            })}
+          >
+            Rouge
+          </div>
+          <div>By Mona Awad</div>
+        </div>
+        <IonIcon size="large" icon={arrowForwardCircleOutline} />
+      </div>
+    </div>
   );
 };
 
